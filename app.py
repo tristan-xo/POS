7cda34250393a098beacb19b639ae91a649727af
tristan-xo <arun2003kumar.ak47@gmail.com>
2025-04-22 10:39:53 +0530

    arun

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ def get_current_stock():
         return jsonify({"error": "Server error processing stock data"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
